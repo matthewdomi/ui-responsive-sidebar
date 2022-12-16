ffedc9c98b1e3261f0ed7e707ea6321ced7c51c6
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-12-16 12:53:28 +0000

    df

---
+++ components/Admin/Dashboard/StatisticsCard.js
@@ -0,0 +1,116 @@
+import {
+  Box,
+  Grid,
+  Card,
+  Avatar,
+  CardHeader,
+  IconButton,
+  Typography,
+  CardContent,
+} from '@mui/material';
+
+import {
+  TrendingUp,
+  CurrencyUsd,
+  DotsVertical,
+  CellphoneLink,
+  AccountOutline,
+} from 'mdi-material-ui';
+
+const salesData = [
+  {
+    stats: '245k',
+    title: 'Sales',
+    color: 'primary',
+    icon: <TrendingUp sx={{ fontSize: '1.75rem' }} />,
+  },
+  {
+    stats: '12.5k',
+    title: 'Customers',
+    color: 'success',
+    icon: <AccountOutline sx={{ fontSize: '1.75rem' }} />,
+  },
+  {
+    stats: '1.54k',
+    color: 'warning',
+    title: 'Products',
+    icon: <CellphoneLink sx={{ fontSize: '1.75rem' }} />,
+  },
+  {
+    stats: '$88k',
+    color: 'info',
+    title: 'Revenue',
+    icon: <CurrencyUsd sx={{ fontSize: '1.75rem' }} />,
+  },
+];
+
+const renderStats = () => {
+  return salesData.map((item, index) => (
+    <Grid item xs={12} sm={3} key={index}>
+      <Box key={index} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar
+          variant="rounded"
+          sx={{
+            mr: 3,
+            width: 44,
+            height: 44,
+            boxShadow: 3,
+            color: 'common.white',
+            backgroundColor: `${item.color}.main`,
+          }}
+        >
+          {item.icon}
+        </Avatar>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography variant="caption">{item.title}</Typography>
+          <Typography variant="h6">{item.stats}</Typography>
+        </Box>
+      </Box>
+    </Grid>
+  ));
+};
+
+const StatisticsCard = () => {
+  return (
+    <Card>
+      <CardHeader
+        title="Statistics Card"
+        action={
+          <IconButton
+            size="small"
+            aria-label="settings"
+            className="card-more-options"
+            sx={{ color: 'text.secondary' }}
+          >
+            <DotsVertical />
+          </IconButton>
+        }
+        subheader={
+          <Typography variant="body2">
+            <Box
+              component="span"
+              sx={{ fontWeight: 600, color: 'text.primary' }}
+            >
+              Total 48.5% growth
+            </Box>{' '}
+            😎 this month
+          </Typography>
+        }
+        titleTypographyProps={{
+          sx: {
+            mb: 2.5,
+            lineHeight: '2rem !important',
+            letterSpacing: '0.15px !important',
+          },
+        }}
+      />
+      <CardContent sx={{ pt: (theme) => `${theme.spacing(3)} !important` }}>
+        <Grid container spacing={[5, 0]}>
+          {renderStats()}
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default StatisticsCard;
